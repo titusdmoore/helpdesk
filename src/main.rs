6c4afd31b4c2f4a6e266c5bd3b8dd84b6fe01945
Titus Moore <tmoore@edgewebware.com>
2023-05-10 18:21:48 -0400

    Worked on loop and controls

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,12 @@ use crossterm::{
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
-use std::{io, thread, time::Duration};
+use std::{
+    io,
+    sync::mpsc,
+    thread,
+    time::{Duration, Instant},
+};
 use tui::{
     backend::CrosstermBackend,
     layout::{Constraint, Direction, Layout},
@@ -13,6 +18,28 @@ use tui::{
 };
 
 fn main() -> Result<(), io::Error> {
+    let (tx, rx) = mpsc::channel();
+    let tick_rate = Duration::from_millis(200);
+
+    thread::spawn(move || {
+        let mut last_tick = Instant::now();
+        loop {
+            let timeout = tick_rate
+                .checked_sub(last_tick.elapsed())
+                .unwrap_or_else(|| Duration::from_secs(0));
+
+            if event::poll(timeout).unwrap() {
+                if let Event::Key(key) = event::read().unwrap() {
+                    tx.send(key).unwrap();
+                }
+            }
+
+            if last_tick.elapsed() >= tick_rate {
+                last_tick = Instant::now();
+            }
+        }
+    });
+
     // setup terminal
     enable_raw_mode()?;
     let mut stdout = io::stdout();
@@ -20,19 +47,27 @@ fn main() -> Result<(), io::Error> {
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
 
-    terminal.draw(|f| {
-        let size = f.size();
+    loop {
+        terminal.draw(|f| {
+            let size = f.size();
 
-        let text = Text::raw("Welcome to the Titus Moore Help Desk!");
-        let paragraph = Paragraph::new(text)
-            .block(Block::default().title("Help Desk").borders(Borders::ALL))
-            .wrap(Wrap { trim: true });
+            let text = Text::raw("Welcome to the Titus Moore Help Desk!");
+            let paragraph = Paragraph::new(text)
+                .block(Block::default().title("Help Desk").borders(Borders::ALL))
+                .wrap(Wrap { trim: true });
 
-        f.render_widget(paragraph, size);
-    })?;
+            f.render_widget(paragraph, size);
+        })?;
+    }
 
-    thread::sleep(Duration::from_millis(5000));
+    clean_terminal(&mut terminal)
+}
 
+fn clean_terminal<B>(terminal: &mut Terminal<B>) -> Result<(), io::Error>
+where
+    B: tui::backend::Backend,
+    B: std::io::Write,
+{
     // restore terminal
     disable_raw_mode()?;
     execute!(
